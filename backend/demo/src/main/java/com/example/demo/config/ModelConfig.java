a2a477ed5d9a8bbe76b51c5df0879cb23806c70f
edaywalid <becharwalid9@gmail.com>
2024-03-21 14:52:03 +0100

    fix

--- backend/demo/src/main/java/com/example/demo/config/ModelConfig.java
+++ backend/demo/src/main/java/com/example/demo/config/ModelConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/app")
+@RequestMapping("/api/v1/app")
 @CrossOrigin(origins = "http://localhost:5173")
 public class ModelConfig {
 
