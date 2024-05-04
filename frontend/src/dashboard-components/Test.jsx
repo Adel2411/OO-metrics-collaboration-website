18d9db040df6e506f20a6c3010ec3b9c9c245ce0
Adel2411 <hadjarabadel.2411@gmail.com>
2024-05-04 19:26:06 +0100

    Finished Test.jsx Component

--- frontend/src/dashboard-components/Test.jsx
+++ frontend/src/dashboard-components/Test.jsx
@@ -1,6 +1,7 @@
 import { useCallback , useState} from 'react';
 import { useDropzone } from 'react-dropzone';
 
+
 function Test() {
     const [files, setFiles] = useState([]);
     const onDrop = useCallback((acceptedFiles) => {
@@ -11,7 +12,13 @@ function Test() {
         });
     }, [files]);
 
+    function HandleSubmit(e) {
+        e.preventDefault();
+        console.log(files);
+    }
+
     const { getRootProps, getInputProps } = useDropzone({ onDrop, noClick: files.length > 0});
+    const { getRootProps: getButtonProps, getInputProps: getButtonInputProps } = useDropzone({ onDrop, noClick: false, noKeyboard: true });
 
     const deleteFile = (path) => {
         setFiles(files.filter(file => file.path !== path));
@@ -22,30 +29,39 @@ function Test() {
             <div className="flex justify-center h-1/4 sm:h-1/3">
                 <h1 className="component-title pt-4 underline">Test</h1>
             </div>
-            <div className="h-3/4 sm:h-2/3 flex flex-col items-center gap-10 sm:gap-5">
-                <div className="h-1/2 w-full md:w-1/2 border-first bg-second border-4 rounded-box">
-                {files.length === 0 ? (
-                    <div {...getRootProps(
-                        {className: "h-full dropzone flex flex-col justify-center items-center"}
-                    )}>
-                        <input {...getInputProps()} directory="" webkitdirectory=""/>
-                        <p>Drag the package to test...</p>
+            <div className="px-10 lg:px-36 h-3/4 sm:h-2/3 flex flex-col items-center gap-10 lg:gap-5">
+                    <div className="overflow-y-auto h-1/2 w-full border-first bg-second border-4 rounded-box">
+                        {files.length === 0 ? (
+                            <div {...getRootProps(
+                                {className: "h-full dropzone flex flex-col justify-center items-center"}
+                            )}>
+                                <input {...getInputProps()} directory="" webkitdirectory=""/>
+                                <p className="text-xs sm:text-lg">Drag the package to test...</p>
+                            </div>
+                        ) : (
+                            <div className="flex flex-col w-full items-center gap-10 p-4">
+                                <ul {...getRootProps(
+                                    {className: "flex flex-col items-center gap-1 h-full w-full"}
+                                )}>
+                                    <input {...getInputProps()} directory="" webkitdirectory=""/>
+                                    {files.map((file, index) => (
+                                        <li key={index}
+                                            className="text-xs lg:text-sm lg:h-16 flex justify-between items-center w-full rounded-box bg-first p-5 h-10">
+                                            <div
+                                                className="overflow-hidden text-overflow-ellipsis whitespace-nowrap">{file.name}</div>
+                                            <button className="btn btn-ghost" onClick={() => deleteFile(file.path)}>🗑️
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                                <button {...getButtonProps({className: "btn bg-third hover:bg-fourth 1/3 lg:w-1/4 text-white"})}>
+                                    <input {...getButtonInputProps()} directory="" webkitdirectory=""/>
+                                    Add More Files
+                                </button>
+                            </div>
+                        )}
                     </div>
-                ) : (
-                    <ul {...getRootProps(
-                        {className: "flex flex-col items-center gap-2 h-full overflow-y-auto"}
-                    )}>
-                        <input {...getInputProps()} directory="" webkitdirectory=""/>
-                        {files.map((file, index) => (
-                            <li key={index} className="flex justify-between items-center w-full rounded-box bg-first p-5">
-                                <div>{file.name}</div>
-                                <button className="btn btn-ghost" onClick={() => deleteFile(file.path)}>🗑️</button>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-                </div>
-                <button className="btn btn-primary w-1/3 text-white">Submit</button>
+                <button onClick={HandleSubmit} className="btn w-1/3 text-white bg-first hover:bg-second">Submit</button>
             </div>
         </div>
     );
