245b6d5cb3f7e9b76f9e92f38c57fd55ad20dba4
edaywalid <becharwalid9@gmail.com>
2024-05-22 20:53:20 +0100

    final update

--- backend/demo/src/main/java/com/example/demo/MetricAnalyser/MetricDB/NumberOfImportedClasses.java
+++ backend/demo/src/main/java/com/example/demo/MetricAnalyser/MetricDB/NumberOfImportedClasses.java
@@ -4,10 +4,7 @@ import com.example.demo.MetricAnalyser.MetricResultModel;
 import com.example.demo.MetricAnalyser.MetricStructure;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,12 +21,42 @@ public class NumberOfImportedClasses extends MetricStructure {
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
                 if (line.startsWith("import ")) {
-                    String packageName = line.substring(7).replace(";", "").trim();
-                    packages.add(packageName);
+                    String packageName = line.substring(7, line.length() - 1); // 7 is the length of "import "
+                    if (packageName.endsWith(".*")) {
+                        String rootPackage = packageName.substring(0, packageName.length() - 2);
+                        addClassesOfPackage(rootPackage, packages);
+                        // addSubPackages(packages, rootPackage);   // Uncomment this line to include the
+                        // subpackages
+                        // packages.add(rootPackage);   // Uncomment this line to include the root package in
+                        // the imported packages
+                    } else {
+                        packages.add(packageName.replace(".", "/"));
+                    }
                 }
             }
         }
         return packages;
+    }
+
+    public void addClassesOfPackage(String packageName, Set<String> importedPackages) {
+        String packagePath = packageName.replace('.', '/');
+        String javaHome = System.getProperty("java.home");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(javaHome + "/lib/classlist"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (line.startsWith(packagePath) && !line.contains("$")) {
+                    line = line.substring(packagePath.length() + 1);
+                    if (!line.contains("/")) {
+                        importedPackages.add(line);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
